96c01a14ec0f02e1f50ff46162bed964a2b94e62
aayushourshopee <aayush.verma@ourshopee.com>
2025-11-10 17:36:03 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -36,6 +36,19 @@ const nextConfig = {
     };
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
