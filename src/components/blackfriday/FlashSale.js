502b35d706b43f1eb71e52097f5178817ae46014
himanshu chaturvedi <himanshu.chaturvedi@ourshopee.com>
2025-11-12 18:04:43 +0530

    Banner Section completed

--- src/components/blackfriday/FlashSale.js
+++ src/components/blackfriday/FlashSale.js
@@ -1,26 +1,28 @@
-import CountdownTimer from "../homepage/CountdownTimer";
+"use client"
+import { useSelector } from "react-redux";
+import { CarouselWithBanner } from "../homepage";
+import CountdownClock from "../homepage/CountdownClock";
 
 const FlashSale = () => {
+      const top_picks = useSelector((state) => state?.homeslice?.top_picks);
   return (
     <div>
-      <div className="component_1 mt-4">
+      <div className="component_1 mt-4 p-4">
         <div className="component_header">
           <div>
-            <img src="" className="" />
+            <img src="/assets/black-friday/flash-sale.gif" className="" />
           </div>
 
-          <CountdownTimer />
+          <CountdownClock endDate={new Date("2025-11-22")} labelColor={"#fff"} />
         </div>
 
-        {!loading5 && (
-          <CarouselWithBanner
-            products={top_picks?.[0]?.productlist}
-            bannerImage={top_picks?.[0]?.image_slider}
-            bannerImageRedirectUrl={top_picks?.[0]?.url}
-            type={1}
-            inner_bg={"rgba(238, 235, 250, 1)"}
-          />
-        )}
+        <CarouselWithBanner
+          products={top_picks?.[0]?.productlist}
+          bannerImage={top_picks?.[0]?.image_slider}
+          bannerImageRedirectUrl={top_picks?.[0]?.url}
+          type={1}
+          inner_bg={"rgba(238, 235, 250, 1)"}
+        />
       </div>
     </div>
   );
